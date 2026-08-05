067be1b798a49050e72f51061764671a33f6ceba
シャムスル <99482630+shamsul404@users.noreply.github.com>
2026-08-05 08:47:27 +0000

    Update _decorators.py

--- pyUltroid/_misc/_decorators.py
+++ pyUltroid/_misc/_decorators.py
@@ -108,7 +108,7 @@ def ultroid_cmd(
             chat = ult.chat
             if hasattr(chat, "title"):
                 if (
-                    "#noub" in chat.title.lower()
+                    "#181" in chat.title.lower()
                     and not (chat.admin_rights or chat.creator)
                     and not (ult.sender_id in DEVLIST)
                 ):
